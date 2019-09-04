2894743024a1f1eb3970e4f13ff436821ba41ce6
Rafael Duarte <rafael@aproximma.com.br>
2019-09-04 12:45:20 -0300

    init project

---
+++ Controllers/TokenController.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using Microsoft.Rest;
+using System.Text;
+
+namespace allegro_pbi_token_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        // GET api/values
+        [HttpPost]
+        public async Task<ActionResult<TokenCredentials>> PostAsync()
+        {
+          using (HttpClient client = new HttpClient())
+          {
+            string tokenEndpoint = "https://login.microsoftonline.com/common/oauth2/token";
+            string accept = "application/json";
+            string userName = "allegro@segmentopesquisas.com.br";
+            string password = "Q2w12ssw!!";
+            string clientId = "837c3d7f-b43f-4e51-a351-e9a3c8e2d64a";
+
+            client.DefaultRequestHeaders.Add("Accept", accept);
+            string postBody = null;
+
+            postBody = $@"resource=https%3A%2F%2Fanalysis.windows.net/powerbi/api
+                            &client_id={clientId}
+                            &grant_type=password
+                            &username={userName}
+                            &password={password}
+                            &scope=openid";
+
+            var tokenResult = await client.PostAsync(tokenEndpoint, new StringContent(postBody, Encoding.UTF8, "application/x-www-form-urlencoded"));
+            tokenResult.EnsureSuccessStatusCode();
+            var tokenData = await tokenResult.Content.ReadAsStringAsync();
+
+            JObject parsedTokenData = JObject.Parse(tokenData);
+
+            var token = parsedTokenData["access_token"].Value<string>();
+            TokenCredentials tokenCredentials = new TokenCredentials(token, "Bearer");
+            return tokenCredentials;
+          }
+        }
+    }
+}
